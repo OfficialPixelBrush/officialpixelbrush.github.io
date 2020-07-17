e184fe7975db6bacbbb959e97d76a1effbcab36f
Pixel Brush <letsplaytvirtmann@gmail.com>
2020-07-17 19:38:03 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
 let nmbr_of_posts = 17;
 let nmbr = 17;
 
+let latestcommit="";
+
+ function componentDidMount() {
+    // Replace this with your own repo
+    // https://api.github.com/repos/:owner/:repo/branches/master
+    fetch(
+      "https://api.github.com/repos/OfficialPixelBrush/officialpixelbrush.github.io/branches/master"
+    )
+      .then(response => {
+        response.json().then(json => {
+          console.log(json);
+          this.setState({
+            author: json.commit.author.login,
+            branch: json.name,
+            date: json.commit.commit.author.date,
+            sha: json.commit.sha,
+            link: json._links.html
+          });
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+	  
+	  latestcommit=this.state.date;
+}
+
 function FileHelper(pathOfFileToReadFrom) {
     var request = new XMLHttpRequest();
     request.open("GET", pathOfFileToReadFrom, false);
