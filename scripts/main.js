c66b0e6797ac4931ef57f1fe0e3e6e8d13ff164c
Pixel Brush <letsplaytvirtmann@gmail.com>
2020-07-17 13:21:31 +0200

    Readded main.js

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let nmbr_of_posts = 15;
+let nmbr = 15;
+
+function FileHelper(pathOfFileToReadFrom) {
+    var request = new XMLHttpRequest();
+    request.open("GET", pathOfFileToReadFrom, false);
+    request.send(null);
+    var returnValue = request.responseText;
+
+    return returnValue;
+}
+
+function loadPost() {
+	document.getElementById('mainpost').innerHTML = FileHelper('posts/' + nmbr + '.txt');
+	document.getElementById('mainpost2').innerHTML = FileHelper('posts/' + nmbr + '.txt');
+	document.getElementById('mainpost3').innerHTML = FileHelper('posts/' + nmbr  + '.txt');
+}
+
+function next() {
+	if (nmbr+3 <= nmbr_of_posts) {
+		nmbr = nmbr+3
+		loadPost();
+	}
+}
+
+function previous() {
+	if (nmbr-3 > 0) {
+		nmbr = nmbr-3
+		loadPost();
+	}
+}
