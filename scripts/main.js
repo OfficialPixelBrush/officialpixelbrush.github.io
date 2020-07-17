f19a2746b370f50a36f5d61367466f78705aae55
Pixel Brush <letsplaytvirtmann@gmail.com>
2020-07-17 14:38:40 +0200

    fix x2

--- scripts/main.js
+++ scripts/main.js
@@ -11,27 +11,27 @@ function FileHelper(pathOfFileToReadFrom) {
 }
 
 function loadPost() {
-	if (nbmr > 0) {
+	if (nmbr > 0) {
 		document.getElementById('mainpost').innerHTML = FileHelper('posts/' + nmbr + '.txt');
 	} else {
 		document.getElementById('mainpost').innerHTML = '-';
 	}
-	if (nbmr-1 > 0) {
+	if (nmbr-1 > 0) {
 		document.getElementById('mainpost2').innerHTML = FileHelper('posts/' + (nmbr-1) + '.txt');
 	} else {
 		document.getElementById('mainpost2').innerHTML = '-';
 	}
-	if (nbmr-2 > 0) {
+	if (nmbr-2 > 0) {
 		document.getElementById('mainpost3').innerHTML = FileHelper('posts/' + (nmbr-2)  + '.txt');
 	} else {
 		document.getElementById('mainpost3').innerHTML = '-';
 	}
-	if (nbmr-3 > 0) {
+	if (nmbr-3 > 0) {
 		document.getElementById('mainpost4').innerHTML = FileHelper('posts/' + (nmbr-3)  + '.txt');
 	} else {
 		document.getElementById('mainpost4').innerHTML = '-';
 	}
-	if (nbmr-4 > 0) {
+	if (nmbr-4 > 0) {
 		document.getElementById('mainpost5').innerHTML = FileHelper('posts/' + (nmbr-4)  + '.txt');
 	} else {
 		document.getElementById('mainpost5').innerHTML = '-';
