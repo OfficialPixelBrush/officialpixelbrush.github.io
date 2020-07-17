dd2162f98212ab05935c984fce3d41d190326c22
Pixel Brush <letsplaytvirtmann@gmail.com>
2020-07-17 14:19:12 +0200

    Homepage finally reworked

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-let nmbr_of_posts = 15;
-let nmbr = 15;
+let nmbr_of_posts = 16;
+let nmbr = 16;
 
 function FileHelper(pathOfFileToReadFrom) {
     var request = new XMLHttpRequest();
@@ -14,18 +14,20 @@ function loadPost() {
 	document.getElementById('mainpost').innerHTML = FileHelper('posts/' + nmbr + '.txt');
 	document.getElementById('mainpost2').innerHTML = FileHelper('posts/' + (nmbr-1) + '.txt');
 	document.getElementById('mainpost3').innerHTML = FileHelper('posts/' + (nmbr-2)  + '.txt');
+	document.getElementById('mainpost4').innerHTML = FileHelper('posts/' + (nmbr-3)  + '.txt');
+	document.getElementById('mainpost5').innerHTML = FileHelper('posts/' + (nmbr-4)  + '.txt');
 }
 
 function next() {
-	if (nmbr+3 <= nmbr_of_posts) {
-		nmbr = nmbr+3
+	if (nmbr+5 <= nmbr_of_posts) {
+		nmbr = nmbr+5
 		loadPost();
 	}
 }
 
 function previous() {
-	if (nmbr-3 > 0) {
-		nmbr = nmbr-3
+	if (nmbr-5 > 0) {
+		nmbr = nmbr-5
 		loadPost();
 	}
 }
