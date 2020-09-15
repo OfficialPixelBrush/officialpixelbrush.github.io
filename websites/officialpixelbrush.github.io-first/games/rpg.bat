6e0627d42fe4e9a86117a47dc3617af65c97968b
Pixel Brush <letsplaytvirtmann@gmail.com>
2020-09-15 21:37:29 +0200

    added pixelbrush website v1

---
+++ websites/officialpixelbrush.github.io-first/games/rpg.bat
@@ -0,0 +1,30 @@
+﻿@echo off
+chcp 65001
+@chcp 65001>nul
+
+:titlescreen
+cls
+echo  █ █ █ █ █ █ █ █ █ █ 
+echo  █ R P G   G A M E  █
+echo  █ █ █ █ █ █ █ █ █ █
+echo   School  Edition
+pause
+
+:playscreen
+cls
+echo + Play +
+echo - End  -
+choice /c 12 /n
+if %errorlevel%==1 goto l1
+if %errorlevel%==2 goto endscreen
+
+:endscreen
+cls
+echo - Play -
+echo + End  +
+choice /c 12 /n
+if %errorlevel%==1 goto playscren
+if %errorlevel%==2 goto shutdown+
+
+:shutdown
+exit
