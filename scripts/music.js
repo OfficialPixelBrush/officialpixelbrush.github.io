0029d268b829367dba2e86d06d258525011122cd
OfficialPixelBrush <letsplaytvirtann@gmail.com>
2021-02-17 18:11:18 +0100

    I'm just adding these so GitHub Desktop looks tidy again

---
+++ scripts/music.js
@@ -0,0 +1,16 @@
+function FileHelper(pathOfFileToReadFrom) {
+    var request = new XMLHttpRequest();
+    request.open("GET", pathOfFileToReadFrom, false);
+    request.send(null);
+    var returnValue = request.responseText;
+
+    return returnValue;
+}
+
+function loadYear(year) {
+	if (year != null) {
+		document.getElementById('selected').innerHTML = FileHelper('music/' + year + '.txt');
+	} else {
+		document.getElementById('selected').innerHTML = '-';
+	}
+}
