ab2b2e482d84196340d70e724a64a566cc7c2923
OfficialPixelBrush <letsplaytvirtann@gmail.com>
2021-08-16 13:08:35 +0200

    a pain

---
+++ atscraft/scripts/countdown.js
@@ -0,0 +1,10 @@
+const now = new Date();
+const d = new Date('September 05, 2021 12:00:00');
+
+const time = d.getTime() - now.getTime();
+const days = Math.floor(time / 1000 / 60 / 60 / 24);
+//const hours = Math.floor(time / 1000 / 60 / 60) % 24;
+//const minutes = Math.floor(time / 1000 / 60) % 60;
+//const seconds = Math.floor(time / 1000) % 60;
+let final = days.toString() + " Days until...\n";
+document.getElementById('countdownDIV').innerHTML = final;
